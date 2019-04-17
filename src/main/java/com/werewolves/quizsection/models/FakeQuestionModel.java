d74ba225cfb68e62c8f9cb91cdca47bdf36a7e5d
Muhammed Eldeeb <meldeeb1998@gmail.com>
2019-04-17 18:46:46 +0200

    question dealing with fake db

--- src/main/java/com/werewolves/quizsection/models/FakeQuestionModel.java
+++ src/main/java/com/werewolves/quizsection/models/FakeQuestionModel.java
@@ -5,6 +5,7 @@ import com.werewolves.quizsection.entities.Question;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,10 +19,10 @@ public class FakeQuestionModel extends QuestionModel {
     static {
         questions = new HashMap<Integer, Question>(){
             {
-                put(1 , new Question(1 , "What?"));
-                put(2 , new Question(2 , "Where?"));
-                put(3 , new Question(3 , "How?"));
-                put(4 , new Question(4 , "Who?"));
+                put(1 , new Question(1 , "What?" , 1));
+                put(2 , new Question(2 , "Where?",1));
+                put(3 , new Question(3 , "How?" , 2));
+                put(4 , new Question(4 , "Who?" , 3));
             }
         };
     }
@@ -33,37 +34,61 @@ public class FakeQuestionModel extends QuestionModel {
     }
 
     @Override
-    public Collection<Question> getAllQuestions() {
-        return questions.values();
+    public Collection<Question> getAllQuestions(int quizId) {
+        Collection<Question> temp = new ArrayList<>();
+        for(Integer key : questions.keySet()){
+            Question value = questions.get(key);
+            if(value.getQuizId() == quizId){
+                temp.add(value);
+            }
+        }
+        return temp;
     }
 
     @Override
-    public Question getQuestionByID(int id) {
-        return questions.get(id);
+    public Question getQuestionByID(int quizId, int id) {
+        for(Integer key : questions.keySet()){
+            Question value = questions.get(key);
+            if(value.getQuizId() == quizId && key == id){
+                return value;
+            }
+        }
+        return null;
     }
 
     @Override
-    public Question updateQuestionByID(Question newQuestion) {
+    public Question insertQuestion(int quizId, Question question) {
+        question.setQuizId(quizId);
+        questions.put(question.getId() , question);
+        Question newQ = this.questions.get(question.getId());
+        return newQ;
+    }
+
+    @Override
+    public Question deleteQuestionByID(int quizId, int id) {
+        Question temp = null;
+        for(Integer key : questions.keySet()){
+            Question value = questions.get(key);
+            if(key == id && value.getQuizId() == quizId){
+                temp = value;
+                questions.remove(key);
+                break;
+            }
+        }
+        return temp;
+    }
+
+    @Override
+    public Question updateQuestionByID(int quizId, Question newQuestion) {
         Question old = questions.get(newQuestion.getId());
         old.setCorrectChoiceID(newQuestion.getCorrectChoiceID());
         old.setChoices(newQuestion.getChoices());
         old.setTitle(newQuestion.getTitle());
+        old.setQuizId(quizId);
 
         this.questions.put(old.getId() , old);
         return old;
     }
 
-    @Override
-    public Question deleteQuestionByID(int id) {
-        Question deleted = this.questions.get(id);
-        this.questions.remove(id);
-        return deleted;
-    }
 
-    @Override
-    public Question insertQuestion(Question question) {
-        this.questions.put(question.getId() , question);
-        Question newQ = this.questions.get(question.getId());
-        return newQ;
-    }
 }
--- src/main/java/com/werewolves/quizsection/models/QuestionModel.java
+++ src/main/java/com/werewolves/quizsection/models/QuestionModel.java
@@ -7,13 +7,13 @@ import java.util.Collection;
 public abstract class QuestionModel extends Model {
     public abstract int add(Question question);
 
-    public abstract Collection<Question> getAllQuestions();
+    public abstract Collection<Question> getAllQuestions(int quizId);
 
-    public abstract Question getQuestionByID(int id);
+    public abstract Question getQuestionByID(int quizId, int id);
 
-    public abstract Question updateQuestionByID(com.werewolves.quizsection.entities.Question question);
+    public abstract Question updateQuestionByID(int quizId, Question question);
 
-    public abstract Question deleteQuestionByID(int id);
+    public abstract Question deleteQuestionByID(int quizId, int id);
 
-    public abstract Question insertQuestion(com.werewolves.quizsection.entities.Question question);
+    public abstract Question insertQuestion(int quizId, Question question);
 }

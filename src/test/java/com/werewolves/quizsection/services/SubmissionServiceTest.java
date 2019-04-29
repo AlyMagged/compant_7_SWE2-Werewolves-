94d102c8f20b59f52b1224692aac865bbc2c2253
Muhammed Eldeeb <meldeeb1998@gmail.com>
2019-04-29 13:27:51 +0200

    unit testing for skill service

---
+++ src/test/java/com/werewolves/quizsection/services/SubmissionServiceTest.java
@@ -0,0 +1,28 @@
+package com.werewolves.quizsection.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SubmissionServiceTest {
+
+    @Test
+    public void getAllSubmissions() {
+    }
+
+    @Test
+    public void getSubmissionByID() {
+    }
+
+    @Test
+    public void getSubmissionsForQuiz() {
+    }
+
+    @Test
+    public void getSubmissionsForUser() {
+    }
+
+    @Test
+    public void addSubmission() {
+    }
+}
